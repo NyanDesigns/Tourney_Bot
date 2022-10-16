1d342afa911ab4325d86b42d98e912fd349849f5
NyanDesigns <nmyat@cpp.edu>
2022-10-15 23:15:47 -0700

    8th-commit

---
+++ src/socials.js
@@ -0,0 +1,34 @@
+//Common-Commands
+const commands = {
+	//!socials
+	socials : {
+		response: 'https://linktr.ee/xli24'
+	},
+	//!discord
+	discord : {
+		response: 'https://discord.gg/aNu3pB3QcQ'
+	},
+	//!instagram
+	intagram : {
+		response: 'https://www.instagram.com/xli2499/'
+	},
+	insta : {
+		response: 'https://www.instagram.com/xli2499/'
+	},
+	//!twitter
+	twitter : {
+		response: 'https://twitter.com/xli2499'
+	},
+	tweet : {
+		response: 'https://twitter.com/xli2499'
+	},
+	//!youtube
+	youtube : {
+		response: 'https://www.youtube.com/channel/UCGJwaBY0KRCqT-p_sdMmlwg'
+	},
+	yt : {
+		response: 'https://www.youtube.com/channel/UCGJwaBY0KRCqT-p_sdMmlwg'
+	},
+};
+
+module.exports = {adCommands, commands};
